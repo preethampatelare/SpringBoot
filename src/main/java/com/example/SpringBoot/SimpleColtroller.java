98a9b3d0beedfae2f9f5337157dab90d3efacb02
preethampatelare <areshashipreetham@gmail.com>
2024-02-25 17:06:39 +0530

    created 'hi' API which will greet you

--- src/main/java/com/example/SpringBoot/SimpleColtroller.java
+++ src/main/java/com/example/SpringBoot/SimpleColtroller.java
@@ -1,4 +1,11 @@
 package com.example.SpringBoot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class SimpleColtroller {
+    @GetMapping("/hi")
+    public String sayhi(){
+        return "hello your got Response from server !";
+    }
+
 }
