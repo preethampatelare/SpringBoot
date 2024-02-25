148784669cf102e0cd6e34b1b1850116a2991df0
preethampatelare <areshashipreetham@gmail.com>
2024-02-25 17:22:04 +0530

    sucessfully Done with first API

--- src/main/java/com/example/SpringBoot/SimpleColtroller.java
+++ src/main/java/com/example/SpringBoot/SimpleColtroller.java
@@ -1,7 +1,9 @@
 package com.example.SpringBoot;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class SimpleColtroller {
     @GetMapping("/hi")
     public String sayhi(){
