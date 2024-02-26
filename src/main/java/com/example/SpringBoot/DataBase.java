0909bad1c1b886b74c943015379c3006cbf3b9d0
preethampatelare <areshashipreetham@gmail.com>
2024-02-27 02:48:31 +0530

    completly learnt Get API

---
+++ src/main/java/com/example/SpringBoot/DataBase.java
@@ -0,0 +1,34 @@
+package com.example.SpringBoot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class DataBase {
+    private List<User> list = new ArrayList<>();
+    public DataBase(){
+        list.add(new User(1,"Shashi",22));
+        list.add(new User(2,"Preetham",23));
+        list.add(new User(3,"Kodanla",42));
+        list.add(new User(4,"U shekar",41));
+    }
+    public List<User> allUsers(){
+        return list;
+    }
+    public User returnByName(String name){
+        for(User u : list){
+            if(u.getName().equals(name)){
+                return u;
+            }
+        }
+        return null;
+    }
+    public User returnById(int id){
+        for(User u : list){
+            if(u.getId() == id){
+                return u;
+            }
+        }
+        return null;
+    }
+}
--- src/main/java/com/example/SpringBoot/SimpleColtroller.java
+++ src/main/java/com/example/SpringBoot/SimpleColtroller.java
@@ -1,13 +1,40 @@
 package com.example.SpringBoot;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 
 @RestController
 public class SimpleColtroller {
+    // http://localhost:8080/hi
     @GetMapping("/hi")
     public String sayhi(){
         return "hello your got Response from server !";
     }
 
+    // http://localhost:8080/search?q=name
+    DataBase db = new DataBase();
+
+    // http://localhost:8080/AllUsers
+    @GetMapping("/AllUsers")
+    public List<User> getAll(){
+        return db.allUsers();
+    }
+    /*@GetMapping("/use")
+    public User getUserBuName(@RequestParam String q){
+        return db.returnByName(q);
+    }*/
+    // http://localhost:8080/user/2
+    @GetMapping("/user/{id}")
+    public User returnById(@PathVariable int id){
+        return db.returnById(id);
+    }
+    @GetMapping("/Users/{name}")
+    public User getUserBuName(@PathVariable String name){
+        return db.returnByName(name);
+    }
 }
